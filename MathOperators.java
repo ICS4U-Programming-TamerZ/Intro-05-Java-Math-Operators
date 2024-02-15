e86926a69a432939b98148001e617ab104a26107
Tamer Zreg <tamer.zreg@stu.ocsb.ca>
2024-02-15 21:46:06 +0000

    Skipping Linter as Asked

--- MathOperators.java
+++ MathOperators.java
@@ -7,81 +7,81 @@
  */
 
 // Imports Utility
-import java.util.*;
+import java.util.Scanner;
 
 // Main Class
-public class MathOperators {
-  // Main Input method
-  public static void main(String[] args) {
-    // Input scanner
-    Scanner sc = new Scanner(System.in);
-    while (true) {
-      // Declaring Variables
-      double FirstNum, SecondNum;
-      double Sum, Difference, Product, Remainder, Power;
-      // Asks user what operation they want to complete
-      System.out.println("What Operation would you like to do? (+, -, *, /, ^, sqrt)");
-      // Gets user input for operation
-      String OperationType = sc.nextLine();
-      try {
-        // Asks user for their first number
-        System.out.print("Enter your First number: ");
-        // Scans first number
-        FirstNum = sc.nextDouble();
-        // Asks user for their second number
-        System.out.print("Enter your Second number: ");
-        // Scans second number
-        SecondNum = sc.nextDouble();
-        // If any error is found
-      } catch (Exception e) {
-        // Display error message
-        System.out.println("Invalid, " + e.getMessage());
-        break;
-      }
-      // If user chose Addition
-      if (OperationType.equals("+")) {
-        // Finds Sum and displays it.
-        Sum = FirstNum + SecondNum;
-        System.out.println("Your Sum for " + FirstNum + " + " + SecondNum + " is " + Sum);
-        break;
-        // If user chose Subtraction
-      } else if (OperationType.equals("-")) {
-        // Finds Difference and displays it.
-        Difference = FirstNum - SecondNum;
-        System.out.println(
-            "Your Difference for " + FirstNum + " - " + SecondNum + " is " + Difference);
-        break;
-        // If user chose Multiplication
-      } else if (OperationType.equals("*")) {
-        // Finds Product and displays it.
-        Product = FirstNum * SecondNum;
-        System.out.println("Your Product for " + FirstNum + " * " + SecondNum + " is " + Product);
-        break;
-        // If user chose Division
-      } else if (OperationType.equals("/")) {
-        // Finds Remainder and displays it.
-        Remainder = FirstNum / SecondNum;
-        System.out.println(
-            "Your Remainder for " + FirstNum + " / " + SecondNum + " is " + Remainder);
-        break;
-        // If user chose power
-      } else if (OperationType.equals("^")) {
-        // Finds Power and displays it.
-        Power = StrictMath.pow(FirstNum, SecondNum);
-        System.out.println("Your Power of " + FirstNum + " ^ " + SecondNum + " is " + Power);
-        break;
-        // If user chose square root
-      } else if (OperationType.equals("sqrt")) {
-        // Finds squareRoot1 and squareRoot2 and displays it.
-        double squareRoot1 = StrictMath.sqrt(FirstNum);
-        double squareRoot2 = StrictMath.sqrt(SecondNum);
-        System.out.println("Square root of " + FirstNum + " is " + squareRoot1);
-        System.out.println("Square root of " + SecondNum + " is " + squareRoot2);
-        break;
-        // If Invalid Input is given
-      } else {
-        System.out.println("Please provide a valid Input");
-      }
+public final class MathOperators {
+    
+    // Private constructor to prevent instantiation
+    private MathOperators() {
+        throw new AssertionError("Utility class should not be instantiated");
     }
-  }
+    
+    // Main Input method
+    public static void main(String[] args) {
+        // Input scanner
+        final Scanner sc = new Scanner(System.in);
+        while (true) {
+            // Declaring Variables
+            double firstNum, secondNum;
+            double sum = 0, difference = 0, product = 0, remainder = 0, power = 0;
+            // Asks user what operation they want to complete
+            System.out.println("What Operation would you like to do? (+, -, *, /, ^, sqrt)");
+            // Gets user input for operation
+            final String operationType = sc.nextLine();
+            try {
+                // Asks user for their first number
+                System.out.print("Enter your First number: ");
+                // Scans first number
+                firstNum = sc.nextDouble();
+                // Asks user for their second number
+                System.out.print("Enter your Second number: ");
+                // Scans second number
+                secondNum = sc.nextDouble();
+            } catch (Exception ex) {
+                // Display error message
+                System.out.println("Invalid input: " + ex.getMessage());
+                break;
+            }
+            // If user chose Addition
+            if (operationType.equals("+")) {
+                // Finds Sum and displays it.
+                sum = firstNum + secondNum;
+                System.out.println("Your Sum for " + firstNum + " + " + secondNum + " is " + sum);
+                break;
+            } else if (operationType.equals("-")) {
+                // Finds Difference and displays it.
+                difference = firstNum - secondNum;
+                System.out.println(
+                        "Your Difference for " + firstNum + " - " + secondNum + " is " + difference);
+                break;
+            } else if (operationType.equals("*")) {
+                // Finds Product and displays it.
+                product = firstNum * secondNum;
+                System.out.println("Your Product for " + firstNum + " * " + secondNum + " is " + product);
+                break;
+            } else if (operationType.equals("/")) {
+                // Finds Remainder and displays it.
+                remainder = firstNum / secondNum;
+                System.out.println(
+                        "Your Remainder for " + firstNum + " / " + secondNum + " is " + remainder);
+                break;
+            } else if (operationType.equals("^")) {
+                // Finds Power and displays it.
+                power = Math.pow(firstNum, secondNum);
+                System.out.println("Your Power of " + firstNum + " ^ " + secondNum + " is " + power);
+                break;
+            } else if (operationType.equals("sqrt")) {
+                // Finds squareRoot1 and squareRoot2 and displays it.
+                double squareRoot1 = Math.sqrt(firstNum);
+                double squareRoot2 = Math.sqrt(secondNum);
+                System.out.println("Square root of " + firstNum + " is " + squareRoot1);
+                System.out.println("Square root of " + secondNum + " is " + squareRoot2);
+                break;
+            } else {
+                System.out.println("Please provide a valid Input");
+            }
+        }
+        sc.close();
+    }
 }
